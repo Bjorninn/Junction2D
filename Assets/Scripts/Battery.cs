0b95692df2746250440cec2118992e9016fefef8
Bjorn <bjorninn@gmail.com>
2016-11-25 21:30:22 +0200

    Added basic battery and flashlight logic

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battery : MonoBehaviour, IPickup
+{
+
+    public float ChargeAmount = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPickUp()
+    {
+        // Get the flashlight
+
+        // Increase the power of the flashlight by ChargeAmount
+        FlashLight.Instance.AddCharge(ChargeAmount);
+
+        // Do something else if we so want
+
+        // Delete the battery
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            OnPickUp();
+        }
+    }
+}
---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour
+{
+    public static FlashLight Instance { private set; get; }
+
+    public float MaxCharge;
+    private float _currentCharge;
+    private float _drainRate;
+
+    private bool _isOn;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Instance = this;
+	    _currentCharge = MaxCharge;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    public void AddCharge(float charge)
+    {
+        _currentCharge += charge;
+
+        if (_currentCharge > MaxCharge)
+        {
+            _currentCharge = MaxCharge;
+        }
+    }
+
+    private IEnumerator Drain()
+    {
+        while (_isOn)
+        {
+            _currentCharge -= _drainRate;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    public void ToggleFlashlight(bool isOn)
+    {
+        _isOn = isOn;
+
+        if (isOn)
+        {
+            StartCoroutine(Drain());
+        }
+    }
+}
