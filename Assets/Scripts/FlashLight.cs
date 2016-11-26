0e4ea7009c6a7cba98823d2ca6def09d721f10e3
Bjorn <bjorninn@gmail.com>
2016-11-26 11:31:03 +0200

    creating the flashlight

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -6,7 +6,8 @@ public class FlashLight : MonoBehaviour
     public static FlashLight Instance { private set; get; }
 
     public float MaxCharge;
-    private float _currentCharge;
+    public Light Beam;
+    public float CurrentCharge { private set; get; }
     private float _drainRate;
 
     private bool _isOn;
@@ -16,22 +17,26 @@ public class FlashLight : MonoBehaviour
 	void Start ()
 	{
 	    Instance = this;
-	    _currentCharge = MaxCharge;
+	    CurrentCharge = MaxCharge;
+	    _drainRate = 10.0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
+	    if (Input.GetKeyDown(KeyCode.F))
+	    {
+	        ToggleFlashlight();
+	    }
 	}
 
     public void AddCharge(float charge)
     {
-        _currentCharge += charge;
+        CurrentCharge += charge;
 
-        if (_currentCharge > MaxCharge)
+        if (CurrentCharge > MaxCharge)
         {
-            _currentCharge = MaxCharge;
+            CurrentCharge = MaxCharge;
         }
     }
 
@@ -39,16 +44,33 @@ public class FlashLight : MonoBehaviour
     {
         while (_isOn)
         {
-            _currentCharge -= _drainRate;
+            CurrentCharge -= _drainRate;
+
+            Debug.Log("Current Charge is: " + CurrentCharge);
+
+            if (CurrentCharge <= 0)
+            {
+                CurrentCharge = 0;
+                ToggleFlashlight();
+            }
+
             yield return new WaitForSeconds(1.0f);
         }
     }
 
-    public void ToggleFlashlight(bool isOn)
+    public void ToggleFlashlight()
     {
-        _isOn = isOn;
+        _isOn = !_isOn;
 
-        if (isOn)
+        Beam.gameObject.SetActive(_isOn);
+
+        if (CurrentCharge <= 0 && _isOn)
+        {
+            // If the player tries to turn the light on but it has no charge
+            // then just turn it off again.
+            ToggleFlashlight();
+        }
+        else if (_isOn)
         {
             StartCoroutine(Drain());
         }
