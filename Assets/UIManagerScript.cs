1ea0c47a697869369f276fdff526395d16ceab04
Peter Leontev <raianknight@gmail.com>
2016-11-26 10:40:27 +0200

    Add basic menu

--- Assets/UIManagerScript.cs
+++ Assets/UIManagerScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class UIManagerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,9 +10,6 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("escape"))
-        {
-            //Instantiate(GameObject.FindObjectOfType<UI)
-        }
-    }
+	
+	}
 }
